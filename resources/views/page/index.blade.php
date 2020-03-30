6cef888e6428ca216023aa584fb2a82b57b9788a
Sanek <djShtaket88@mail.ru>
2020-03-30 10:03:10 +0300

    add scroll false to map yandex

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -82,7 +82,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-8">
-                        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad83036b86e49eab0fc169daacf929b5a150df6588251bc42a8a1161fce9e4f67&amp;source=constructor" width="100%" height="490" frameborder="0"></iframe>
+                        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad83036b86e49eab0fc169daacf929b5a150df6588251bc42a8a1161fce9e4f67&amp;source=constructor&amp;scroll=false" width="100%" height="490" frameborder="0"></iframe>
                     </div>
                     <div class="col-4">
                         @include('layouts.forms.callback')
