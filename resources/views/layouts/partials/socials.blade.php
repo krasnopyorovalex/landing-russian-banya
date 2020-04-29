7bfdf91e7c46ced90173a8f0bbff5a22cc3ae40d
Sanek <djShtaket88@mail.ru>
2020-04-29 10:13:21 +0300

    update social icons

--- resources/views/layouts/partials/socials.blade.php
+++ resources/views/layouts/partials/socials.blade.php
@@ -1,19 +1,19 @@
-<a href="https://m.vk.com/bank_nedvijimosti" target="_blank" rel="noopener noreferrer">
+<a href="https://vk.com/bank_nedvijimosti" target="_blank" rel="noopener noreferrer">
     <svg class="icon">
         <use xlink:href="{{ asset('img/symbols.svg#vk') }}"></use>
     </svg>
 </a>
-{{--<a href="#" target="_blank" rel="noopener noreferrer">--}}
-{{--    <svg class="icon">--}}
-{{--        <use xlink:href="{{ asset('img/symbols.svg#fb') }}"></use>--}}
-{{--    </svg>--}}
-{{--</a>--}}
+<a href="https://www.facebook.com/pages/category/Real-Estate/%D0%91%D0%B0%D0%BD%D0%BA-%D0%9D%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D0%B8-618371035286728/" target="_blank" rel="noopener noreferrer">
+    <svg class="icon">
+        <use xlink:href="{{ asset('img/symbols.svg#fb') }}"></use>
+    </svg>
+</a>
 {{--<a href="#" target="_blank" rel="noopener noreferrer">--}}
 {{--    <svg class="icon">--}}
 {{--        <use xlink:href="{{ asset('img/symbols.svg#ok') }}"></use>--}}
 {{--    </svg>--}}
 {{--</a>--}}
-<a href="https://instagram.com/bank_nedvizhimosti" target="_blank" rel="noopener noreferrer">
+<a href="https://www.instagram.com/bank_nedvizhimosti_/" target="_blank" rel="noopener noreferrer">
     <svg class="icon">
         <use xlink:href="{{ asset('img/symbols.svg#insta') }}"></use>
     </svg>
