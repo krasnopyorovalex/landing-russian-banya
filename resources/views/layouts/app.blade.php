9182020d3801c3c761727424dd8764d026180e65
Sanek <djShtaket88@mail.ru>
2020-05-10 09:43:47 +0300

    add yandex metrika

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -193,7 +193,21 @@
     </div>
     <div id="view__detail-popup" class="popup"></div>
     <div class="popup__show-bg"></div><div class="loader__bg"></div><div class="notify"></div>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+        ym(62689138, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/62689138" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
     <script src="{{ asset('js/jquery.3.3.1.min.js') }}"></script>
     <script src="{{ mix('js/app.min.js') }}"></script>
 </body>
